4aae31fe8f447f6d2df37095a4627a3887d24d1d
xtinalucia <christina.lucia123@gmail.com>
2021-12-18 23:29:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import pastaa from "./pastaa.jpeg";
+
+function Header(props) {
+  console.log(props);
+  return (
+  <header>
+    <h1>{props.name}'s Kitchen</h1>
+  </header>
+  );
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>Serving {props.adjective} Food year round.</p>
+      <img src={pastaa} height={200} alt="bowl of pasta with basil and tomatoes"/>
+<ul style={{textAlign: 'left'}}>
+  {props.dishes.map((dish) => (
+  <li key={dish.id}>{dish.title}</li>))}
+</ul>
+    </section>
+  );
+}
+
+
+function Footer(props) {
+  return (
+  <footer>
+    <p>Copyright {props.year}</p>
+  </footer>
+  );
+}
+
+
+const dishes= [
+  "mac + cheese",
+  "Salmon + Grits",
+  "Tofu + Veg",
+  "Pumpkin Rav + Sage",
+  "Tomato + Garlic Pasta"
+];
+
+const dishObjects= dishes.map((dish, i) => ({id:i, title:dish}));
+console.log(dishObjects);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Marcie" />
+      <Main adjective="Unworldly" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
